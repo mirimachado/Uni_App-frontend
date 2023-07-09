0f295fdf146cc3bb25f7c2a46836313b22edcfea
miri <mirii.machadoo.c@gmail.com>
2023-07-09 01:17:36 -0300

    alterações iniciais, servidor, rotas e nav

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, './models')));
+//app.use('/js', express.static(path.join(__dirname, './models')));
 // =-=-=-=-=-=-=-=-=-=-=-=
 
 // Configurar o servidor para utilizar HandleBars e o Body-Parser
@@ -28,10 +28,37 @@ app.use(bodyparser.json());
 // Configurar o Handlebars
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
 // =-=-=-=-=-=-=-=-=-=-=-=
 
-// Inicializar o servidor
-app.listen(8088, function () {
-    console.log("Servidor rodando na porta 8088!");
+//ROTAS
+
+app.get("/register", function (request, response) {
+    response.render("cadastro");
 });
-// =-=-=-
+
+app.get("/login", function (request, response) {
+    response.render("login");
+});
+
+app.get("/person", function (request, response) {
+    response.render("person");
+});
+
+app.get("/user", function (request, response) {
+    response.render("usuario");
+});
+
+app.get("/smallgroup", function (request, response) {
+    response.render("pgs");
+});
+
+app.get("/home", function (request, response) {
+    response.render("home");
+});
+
+// Inicializar o servidor
+app.listen(8083, function () {
+    console.log("Servidor rodando na porta 8083!");
+});
+// =-=-=-=-=-=-=-=-=-=-=-=
